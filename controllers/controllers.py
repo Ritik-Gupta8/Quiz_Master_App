0df6b309242912d09026d34045601b9bd3e75714
eren-yeager08 <eren2000coder@gmail.com>
2026-01-03 16:02:48 +0530

    changes in controller.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -348,6 +348,7 @@ def submit_quiz(qid, uid, name):
     db.session.commit()
     return redirect(url_for("user_dashboard", uid=uid, name=name))
 
+# View score route for user
 @app.route("/view_score/<uid>/<name>")
 def view_score(uid, name):
     user = User.query.get_or_404(uid) 
