882ee1578cff757ab6d35a63ce4bd282bc225c3d
eren-yeager08 <eren2000coder@gmail.com>
2026-04-30 19:09:04 +0530

    Added tailwind CSS form CDN to CLI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/**/*.js',
+    './static/**/*.css',
+    './**/*.py'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
